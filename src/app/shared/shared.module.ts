bf3e938d19780e63a9eba08fe896e8b5c20aefbc
LuisYagoF <luisyagofdez@gmail.comgit config --global user.email luisyagofdez@gmail.comgit config --global user.email luisyagofdez@gmail.com>
2021-08-12 12:40:41 +0200

    NEW: Research project for Angular custom Directives.

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { ErrMsgDirective } from './directives/err-msg.directive';
+
+
+
+@NgModule({
+  declarations: [
+    ErrMsgDirective
+  ],
+  exports: [
+    ErrMsgDirective
+  ]
+})
+export class SharedModule { }
