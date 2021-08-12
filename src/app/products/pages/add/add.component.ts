1b72e9089af17add287e3ccbaa20eeddb411458c
LuisYagoF <luisyagofdez@gmail.comgit config --global user.email luisyagofdez@gmail.comgit config --global user.email luisyagofdez@gmail.com>
2021-08-12 13:25:17 +0200

    FEAT: input setters in custom directive

--- src/app/products/pages/add/add.component.ts
+++ src/app/products/pages/add/add.component.ts
@@ -12,11 +12,18 @@ export class AddComponent {
   myForm: FormGroup = this.fb.group({
     name: ['', Validators.required]
   })
+  nameErrorMsg: string = 'This is not a valid name'
+  color: string = 'purple'
 
   constructor(private fb: FormBuilder) { }
 
   fieldIsValid(field: string): boolean {
     return this.myForm.get(field)?.invalid || false;
+  }
+
+  changeColor() {
+    const color = "#xxxxxx".replace(/x/g, y => (Math.random() * 16 | 0).toString(16));
+    this.color = color
   }
 
 
--- src/app/shared/directives/err-msg.directive.ts
+++ src/app/shared/directives/err-msg.directive.ts
@@ -6,18 +6,43 @@ import { Directive, ElementRef, Input, OnInit } from '@angular/core';
 export class ErrMsgDirective implements OnInit {
 
   htmlElement: ElementRef<HTMLElement>;
-  @Input() color: string = 'darkred';
+
+  // COMMENTED ---> DIRECTIVE PROPERTIES W/OUT input-SETTERS, ALSO ON SETSTYLE() & SETTEXT()
+  // @Input() text: string = 'This field is required';
+  // @Input() color: string = 'darkred';
+
+  private _text: string = 'This field is required'
+  @Input() set text(value: string) {
+    this.setText()
+  }
+  private _color: string = 'darkred'
+  @Input() set color(value: string) {
+    this._color = value
+    this.setStyle()
+  }
 
   constructor(private el: ElementRef<HTMLElement>) {
     this.htmlElement = el
   }
 
   ngOnInit(): void {
-    this.setColor()
+    this.setClass()
+    this.setStyle()
+    this.setText()
   }
 
-  setColor(): void {
-    this.htmlElement.nativeElement.style.color = this.color
+  setClass(): void {
+    this.htmlElement.nativeElement.classList.add("form-text")
+  }
+
+  setStyle(): void {
+    // this.htmlElement.nativeElement.style.color = this.color
+    this.htmlElement.nativeElement.style.color = this._color
+  }
+
+  setText(): void {
+    // this.htmlElement.nativeElement.innerText = this.text
+    this.htmlElement.nativeElement.innerText = this._text
   }
 
 }
